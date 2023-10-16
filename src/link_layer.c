9a56fc441ab4b834e634a70f91a9771d840ebe4a
andreia <up202108769@g.uporto.pt>
2023-10-16 11:59:49 +0100

    llopen funtion done

--- src/link_layer.c
+++ src/link_layer.c
@@ -1,6 +1,10 @@
 // Link layer protocol implementation
 
 #include "link_layer.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
 
 #define BIT(n) 1 << n
 
@@ -23,15 +27,14 @@ enum linkState{
 #define C_RR(n) ((n << 7) | 0x05)
 #define C_REJ(n) ((n << 7) | 0x01)
 #define C_DISC 0x0B
+#define BAUDRATE B38400
 
 
-int fd;
-
-int txStateMachine(enum linkState *state, LinkLayer *connectionParameters) {
+int txStateMachine(enum linkState *state, int fd) {
     
     unsigned char byte;
     
-    while(state != STOP && connectionParameters->nRetransmissions > 0){
+    while(state != STOP){
         if(read(fd, &byte, 1) > 0){
             switch (*state){
             case START:
@@ -75,12 +78,11 @@ int txStateMachine(enum linkState *state, LinkLayer *connectionParameters) {
                 break;
             }
         }
-        connectionParameters->nRetransmissions--;
     }
 }
 
 
-int rcStateMachine(enum linkState *state) {
+int rcStateMachine(enum linkState *state, int fd) {
     
     unsigned char byte;
     
@@ -131,7 +133,43 @@ int rcStateMachine(enum linkState *state) {
     }
 }
 
+int connectSerialPort(char serialPort[50]){
 
+    int fd = open(serialPort, O_RDWR | O_NOCTTY);
+
+    if (fd < 0){
+        perror(serialPort);
+        return -1;
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if(tcgetattr(fd, &oldtio) == -1){
+        perror("tcgettattr");
+        return -1;
+    }
+
+    // Clean the struct
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0;
+    newtio.c_cc[VMIN] = 5;
+
+    if(tcsetattr(fd, TCSANOW, &newtio) == -1){
+        perror("tcsettattr");
+        return -1;
+    }
+
+    return fd;
+}
 
 ////////////////////////////////////////////////
 // LLOPEN
@@ -140,25 +178,36 @@ int llopen(LinkLayer connectionParameters)
 {
     enum linkState state = START;
 
-    fd = connection(connectionParameters.serialPort);
+    int fd = connectSerialPort(connectionParameters.serialPort);
     if(fd < 0)
         return -1;
 
     switch (connectionParameters.role)
     {
     case LlTx:
-        txStateMachine(&state, &connectionParameters);
+
+        while(connectionParameters.nRetransmissions > 0){
+            unsigned char frame[5] = {FLAG, A_TX, C_SET, (A_TX ^ C_SET), FLAG};
+            int bytes = write(fd, frame, 5);
+            txStateMachine(&state, fd);
+            connectionParameters.nRetransmissions--;
+        }
+        
+        if (state != STOP) return -1;
         break;
 
     case LlRx:
-        rcStateMachine(&state);
+        rcStateMachine(&state, fd);
+        unsigned char frame[5] = {FLAG, A_RC, C_UA, (A_RC ^ C_UA), FLAG};
+        int bytes = write(fd, frame, 5);
         break;
     
     default:
+        return -1;
         break;
     }
 
-    return 1;
+    return fd;
 }
 
 ////////////////////////////////////////////////
